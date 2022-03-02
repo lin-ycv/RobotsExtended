69ac249e0afb9a4e62be7e7e8da64ac60e6035f9
Lin Yu-Chieh (Victor) <victorlin@gapps.ntust.edu.tw>
2022-03-02 15:18:16 +0800

    Add files via upload

---
+++ RobotsExtendedInfo.cs
@@ -0,0 +1,64 @@
+﻿using Grasshopper.Kernel;
+using System;
+using System.Drawing;
+
+namespace RobotsExtended
+{
+    public class RobotsExtendedInfo : GH_AssemblyInfo
+    {
+        public override string Name
+        {
+            get
+            {
+                return "RobotsExtended";
+            }
+        }
+        public override Bitmap Icon
+        {
+            get
+            {
+                return Properties.Resources.iconRobot;
+            }
+        }
+        public override string Description
+        {
+            get
+            {
+                //Return a short string describing the purpose of this GHA library.
+                return "Extension for Robots plugin";
+            }
+        }
+        public override Guid Id
+        {
+            get
+            {
+                return new Guid("1144ad19-a43a-4b2b-870f-19ab431ca98f");
+            }
+        }
+
+        public override string AuthorName
+        {
+            get
+            {
+                return "Victor (Yu Chieh) Lin";
+            }
+        }
+        public override string AuthorContact
+        {
+            get
+            {
+                return "https://github.com/v-xup6/Robots";
+            }
+        }
+
+        static readonly Version v = System.Reflection.Assembly.GetExecutingAssembly().GetName().Version;
+        static internal readonly string Ver = v.Major + "." + v.Minor + "." + v.Build;
+        public override string Version
+        {
+            get
+            {
+                return Ver;
+            }
+        }
+    }
+}
